804d8843b27284e2e8273ab0ce7ca82cb37438fc
IFang Lee <ilee@ifwe.co>
2017-08-28 15:55:22 -0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,105 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Challenge: Rotating Boxes
+ For a harder challenge, make it so that you can use a second finger to rotate your rectangles.
+ To do this, you will need to handle multiple pointers in your MotionEvent handling code.
+ You will also need to rotate your canvas.
+ When dealing with multiple touches, you need these extra ideas:
+ pointer index tells you which pointer in the current set of pointers
+ the event is for pointer ID gives you a unique ID for a specific finger in a gesture
+ The pointer index may change, but the pointer ID will not.
+ For more details, check out the documentation for the following MotionEvent methods:
+ public final int getActionMasked()
+ public final int getActionIndex()
+ public final int getPointerId(int pointerIndex)
+ public final float getX(int pointerIndex)
+ public final float getY(int pointerIndex)
+ Also look at the documentation for the ACTION_POINTER_UP and ACTION_POINTER_DOWN constants.
+ */
+public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
+
+    // Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Paint the boxes a nice semitransparent red (ARGB)
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+
+        // Paint the background off-white
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // Fill the background
+        canvas.drawPaint(mBackgroundPaint);
+
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                // Reset drawing state
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+                mCurrentBox = null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                mCurrentBox = null;
+                break;
+        }
+        Log.i(TAG, action + " at x=" + current.x +
+                ", y=" + current.y);
+        return true;
+    }
+}
